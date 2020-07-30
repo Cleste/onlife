9eb9d3650f62e30edca8ecf91a6e923490e7408a
Cleste <kpimen123@gmail.com>
2020-07-30 19:55:47 +0300

    Code refactored

--- src/main/java/com/onlife/config/EncryptionConfig.java
+++ src/main/java/com/onlife/config/EncryptionConfig.java
@@ -3,13 +3,13 @@ package com.onlife.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class EncryptionConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new Argon2PasswordEncoder(16, 32, 4, 512 * 1024, 16);
+        return new BCryptPasswordEncoder(8);
     }
 }
